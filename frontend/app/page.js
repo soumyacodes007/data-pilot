2e5f29e4ff47e930e7113aa6673260e65a3a060d
Amlan Roy <101693580+Amlan2006@users.noreply.github.com>
2025-04-20 08:45:54 +0530

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <iframe src = "https://homieswork.in" className="w-full h-screen  "/>
+    <iframe src = "https://data-pilot.streamlit.app/" className="w-full h-screen  "/>
     </>
   );
 }
